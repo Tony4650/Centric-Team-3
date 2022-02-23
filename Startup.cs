d29eec34461d1ac4cc12577dd60c7fda5171a6ae
ar808516 <ar808516@HV-I-COB-VX-006>
2022-02-23 10:05:06 -0500

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Centric_Team_3.Startup))]
+namespace Centric_Team_3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
