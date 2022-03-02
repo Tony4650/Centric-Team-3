bcfb95429e03ae799e872b279eb1c9203bba59e5
Joseph Kerstetter <jk000217@ohio.edu>
2022-03-02 10:02:24 -0500

    rebuild DAL

---
+++ DAL/context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Centric_Team_3.Models;
+
+namespace Centric_Team_3.DAL
+{
+    public class context: DbContext
+    {
+        public context() : base("name=DefaultConnection")
+        { 
+        
+        }
+        public DbSet<UserDatabase> UserDatabase { get; set; }
+    }
+}
