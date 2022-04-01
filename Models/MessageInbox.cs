a13cfbc99d94fef2334cdd77db9ab07bdc7efebb
Joseph Kerstetter <jk000217@ohio.edu>
2022-04-01 10:29:22 -0400

    added inbox model

---
+++ Models/MessageInbox.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Centric_Team_3.Models
+{
+    public class MessageInbox
+    {
+        public int inboxID { get; set; }
+
+        public int message { get; set; }
+    }
+}
